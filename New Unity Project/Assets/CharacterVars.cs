ecfa97fd17bd464673908931248ecdb185adf3e4
JTAhlbrecht <55859141+JTAhlbrecht@users.noreply.github.com>
2020-02-01 10:04:04 -0600

    Added Char Vars Objects

---
+++ New Unity Project/Assets/CharacterVars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName="New Char Variables", menuName="Char Vars")]
+public class CharacterVars : ScriptableObject
+{
+    public int affection;
+
+    public bool conv1;
+    public bool conv2;
+    public bool conv3;
+    public bool conv4;
+    public bool conv5;
+    public bool conv6;
+    public bool conv7;
+    public bool conv8;
+    public bool conv9;
+    public bool conv10;
+}
